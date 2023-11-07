ffc6c8a203d42c9c2ba9a5154a5fc8e686c1c0fb
Mohdsohail7 <42858553+Mohdsohail7@users.noreply.github.com>
2023-11-07 14:03:33 +0530

    initial commit

---
+++ DevDetective/index.js
@@ -0,0 +1,206 @@
+
+const searchBar  = document.querySelector(".searchbar-container");
+const profileContainer = document.querySelector(".profile-container");
+const root = document.documentElement.style;
+const get = (param) => document.getElementById(`${param}`);
+const url = "https://api.github.com/users/";
+const noresult = get("no-results");
+const avatar = get("avatar");
+const userName = get("name");
+const user = get("user");
+const date = get("date");
+const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+const bio = get("bio");
+const repos = get("repos");
+const followers = get("followers");
+const following = get("following");
+const user_location = get("location");
+const page = get("page");
+const twitter = get("twitter");
+const company = get("company");
+const btnsubmit = get("submit");
+const input  = get("input");
+let darkMode = false;
+const btnmode = get("btn-mode");
+const modetext = get("mode-text");
+const modeicon = get("mode-icon");
+
+
+
+
+// Event Listeners
+btnsubmit.addEventListener("click", function(){
+    if(input.value !== ""){
+        getProfileDetail(url + input.value);
+    }
+});
+
+input.addEventListener("keydown", function(e){
+    if(e.key == "Enter"){
+        if(input.value !== ""){
+            getProfileDetail(url + input.value);
+        }
+    }
+},
+ false
+);
+
+input.addEventListener("input",function(){
+    noresult.style.display = "none";
+});
+
+btnmode.addEventListener("click", function(){
+    if(darkMode == false){
+        darkModes();
+    }else{
+        lightMode();
+    }
+})
+
+
+// Api function or get the profile detail fun
+
+// async function getProfileDetail(gitUrl){
+//     try {
+//         const response = await fetch(gitUrl);
+//         const data = await response.json();
+//         console.log(data);
+//         updateProfile(data);
+//     } catch (error) {
+//         throw error
+//     }
+    
+// }
+
+// another way to write same fun
+function getProfileDetail(gitUrl){
+    fetch(gitUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            updateProfile(data);
+        })
+        .catch((error) => {
+            throw error;
+        });
+        noresult.classList.add("active");
+
+        setTimeout(() => {
+            noresult.classList.remove("active");
+        }, 4000);
+    
+}
+
+// details display in UI
+
+function updateProfile(data){
+    if(data.message !== "Not Found"){
+        noresult.style.display = "none";
+        function checkNull(param1, param2){
+            if(param1 === "" || param1 === ""){
+                param2.style.opacity = 0.5;
+                param2.previousElementSibling.style.opacity = 0.5;
+                return false;
+            }else{
+                return true;
+            }
+        }
+        avatar.src = `${data.avatar_url}`;
+        userName.innerText = data.name === null ? data.login : data.name;
+        user.innerText = `@${data.login}`;
+        user.href = `${data.html_url}`;
+        datesegments = data.created_at.split("T").shift().split("-");
+        date.innerText = `joined ${datesegments[2]} ${months[datesegments[1] - 1]} ${datesegments[0]}`;
+        bio.innerText = data.bio == null ? "This profile has no bio" : `${data.bio}`;
+        repos.innerText = `${data.public_repos}`;
+        followers.innerText = `${data.followers}`;
+        following.innerText = `${data.following}`;
+        user_location.innerText = checkNull(data.location,user_location) ?  data.location : "Not Available";
+        page.innerText = checkNull(data.blog,page) ? data.blog : "Not Available";
+        page.href = checkNull(data.blog,page) ? data.blog : "#";
+        twitter.innerText = checkNull(data.twitter_username, twitter) ? data.twitter_username : "Not Available";
+        twitter.href = checkNull(data.twitter_username, twitter) ? `https://twitter.com/${data.twitter_username}` : "#";
+        company.innerText = checkNull(data.company, company) ? data.company : "Not Available";
+        searchBar.classList.toggle("active");
+        profileContainer.classList.toggle("active");
+    }
+        else{
+            noresult.style.display = "block";
+        }
+}
+
+
+//dark mode default
+const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+// Toggle between dark and light mode
+function toggleMode() {
+  if (darkMode) {
+    lightMode();
+  } else {
+    darkModes();
+  }
+}
+
+// darkMode enable
+function darkModes(){
+    root.setProperty("--lm-bg", "#141D2F");
+    root.setProperty("--lm-bg-content", "#1E2A47");
+    root.setProperty("--lm-text", "white");
+    root.setProperty("--lm-text-alt", "white");
+    root.setProperty("--lm-shadow-xl", "rgba(70,88,109,0.15)");
+    modetext.innerText = "LIGHT";
+    modeicon.src = "assets/images/sun-icon.svg";
+    root.setProperty("--lm-icon-bg", "brightness(1000%)");
+    darkMode = true;
+    localStorage.setItem("dark-mode", true);
+}
+
+// lightMode enable
+function lightMode(){
+    root.setProperty("--lm-bg", "#F6F8FF");
+    root.setProperty("--lm-bg-content", "#FEFEFE");
+    root.setProperty("--lm-text", "#4B6A9B");
+    root.setProperty("--lm-text-alt", "#2B3442");
+    root.setProperty("--lm-shadow-xl", "rgba(70, 88, 109, 0.25)");
+    modetext.innerText = "DARK";
+    modeicon.src = "assets/images/moon-icon.svg";
+    root.setProperty("--lm-icon-bg", "brightness(100%)");
+    darkMode = false;
+    localStorage.setItem("dark-mode", false);
+}
+
+//initialise UI
+
+
+function initUi(){
+    darkMode = false;
+    const value = localStorage.getItem("dark-mode");
+    console.log(value);
+    if(value === null){
+        console.log("null ke ander " + value)
+        localStorage.setItem("dark-mode", darkMode)
+        lightMode();
+    }
+    else if(value == "true"){
+        console.log("true ke ander " + value)
+        darkModes();
+    }
+    else if(value == "false"){
+        console.log("false ke ander " + value)
+        lightMode();
+    }
+
+    getProfileDetail(url + "Mohdsohail7");
+}
+
+initUi();
+
+    
+
+
+
+
+
+
+
